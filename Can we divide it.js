8d826128efb1f67bd2306465b975fe854b1e273f
Pavel Mamilov <pmamilov@ya.ru>
2022-06-27 16:37:47 +0300

    func is ready

--- Can we divide it.js
+++ Can we divide it.js
@@ -1,6 +1,6 @@
 //Can we divide it?
 function isDivideBy(number, a, b) {
-  return
+  return number % Math.abs(a) === 0 && number % Math.abs(b) === 0;
 }
 
-console.log( isDivideBy(-12, 2, -6) );
+console.log( isDivideBy(-12, 2, -4) );
