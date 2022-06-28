717dfe8b2d958d438d4644178a57eecf35978e10
Pavel Mamilov <pmamilov@ya.ru>
2022-06-28 09:52:01 +0300

    Changed func

--- Expressions Matter.js
+++ Expressions Matter.js
@@ -1,6 +1,6 @@
 //Expressions Matter
 function expressionMatter(a, b, c) {
-  return Math.max(a*(b+c), a*b*c, a+b*c, (a+b)*c, a+b+c);
+  return Math.max(a*(b+c), a*b*c, a+b*c, (a+b)*c, a+b+c, a*b+c);
 }
 
 console.log( expressionMatter(1, 9, 1) );
