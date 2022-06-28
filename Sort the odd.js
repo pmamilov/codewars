9f607899bdab8f29e30d20856dcd4025a5a2dd6a
Pavel Mamilov <pmamilov@ya.ru>
2022-06-28 16:51:49 +0300

    func is ready

--- Sort the odd.js
+++ Sort the odd.js
@@ -1,6 +1,19 @@
 //Sort the odd
 function sortArray(a) {
-  return a
+  let even = a.map(x=> x % 2 === 0 ? x : null);
+  let odd = a.filter(x=> x % 2 !== 0).sort((b,c)=>b-c);
+  let result = [];
+  let j = 0;
+  for(let i of even) {
+    if(i===null) {
+      result.push(odd[j]);
+      ++j;
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result
 }
 
-console.log( sortArray([5, 3, 2, 8, 1, 4]) );
+console.log( sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]) );
