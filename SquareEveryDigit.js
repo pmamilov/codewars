9091a0ca4ca307a248cf43b224dc23e3663eb228
Pavel Mamilov <pmamilov@ya.ru>
2022-06-29 17:47:27 +0300

    add let

--- SquareEveryDigit.js
+++ SquareEveryDigit.js
@@ -1,5 +1,7 @@
 function squareDigits(n){
   return +[...n.toString()].reduce((sum,x)=> sum + String(Math.pow(x, 2)), '');
 }
+let r = squareDigits(3212);
 
-console.log(squareDigits(3212));
+console.log(r);
+
