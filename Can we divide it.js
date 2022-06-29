01f3b60861e81b1a0d51a22a46c86222aeb9d0ce
Pavel Mamilov <pmamilov@ya.ru>
2022-06-29 14:47:32 +0300

    update data

--- Can we divide it.js
+++ Can we divide it.js
@@ -2,5 +2,14 @@
 function isDivideBy(number, a, b) {
   return number % Math.abs(a) === 0 && number % Math.abs(b) === 0;
 }
+console.log(isDivideBy(-12, 2, -4));
 
-console.log( isDivideBy(-12, 2, -4) );
+let i = 'fdd';
+
+switch (expression) {
+  case expression:
+
+    break;
+  default:
+
+}
--- Is this a triangle.js
+++ Is this a triangle.js
@@ -1,8 +1,7 @@
-function isTriangle(a,b,c)
-{
-  [a, b, c] = [a,b,c].sort(((x, y) => x - y));
+function isTriangle(a, b, c) {
+  [a, b, c] = [a, b, c].sort(((x, y) => x - y));
   return a + b > c;
-  
+
 }
 
-console.log( isTriangle(5,5,-2) );
+console.log(isTriangle(5, 5, -2));
