76f892632c96e08a4f87a9013a8c6abfdc167182
Pavel Mamilov <pmamilov@ya.ru>
2022-07-06 09:50:04 +0300

    Add test string

--- Convert string to camel case.js
+++ Convert string to camel case.js
@@ -1,7 +1,8 @@
 //Convert string to camel case
 function toCamelCase(s){
-  return s.replace(/(?<=[-_])\w/g, (e)=> e.toUpperCase()).
-          replace(/[_-]/g, '');
+  return s.replace(/[-_](.)/g, (_, e) => '('+e+')');
+  // return s.replace(/(?<=[-_])\w/g, (e)=> e.toUpperCase()).
+  //         replace(/[_-]/g, '');
 }
 
 console.log( toCamelCase('the-stealth_warrior') );
